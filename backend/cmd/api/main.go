39fc8456b2132e3fda443b1cd43f7b31bb4be2c7
Javier Marcos <1271349+javuto@users.noreply.github.com>
2026-09-18 18:35:34 +0200

    Bump golang version to 1.27.1 and dependencies

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -292,9 +292,8 @@ func mapCTFService() {
 	serviceListener := flagParams.ConfigValues.Service.Listener + ":" + flagParams.ConfigValues.Service.Port
 	if flagParams.ConfigValues.TLS.Termination {
 		cfg := &tls.Config{
-			MinVersion:               tls.VersionTLS12,
-			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-			PreferServerCipherSuites: true,
+			MinVersion:       tls.VersionTLS12,
+			CurvePreferences: []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 			CipherSuites: []uint16{
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
